0cd69098c74913a94cc5bfdc4dd2f825732f4f1d
aman <114105332+Aman-s12345@users.noreply.github.com>
2024-02-10 02:18:21 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -4,10 +4,17 @@ require("dotenv").config();
 const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
-	mongoose
-		.connect( "mongodb+srv://soulopscout:sfaman23@cluster0.druj2gv.mongodb.net/?retryWrites=true&w=majority/DrivingL", {
+	// mongoose
+	// 	.connect( "mongodb+srv://soulopscout:sfaman23@cluster0.druj2gv.mongodb.net/?retryWrites=true&w=majority/DrivingL", {
 			
-		})
+	// 	})
+
+		mongoose.connect("mongodb+srv://soulopscout:sfaman23@cluster0.druj2gv.mongodb.net/DrivingL", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: true,
+  w: "majority"
+});
 		.then(console.log(`DB ka Connection Success`))
 		.catch((err) => {
 			console.log(`DB ka Connection Failed`);
