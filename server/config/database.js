809ca873cf76eca88383ef3a683662f23ac717d4
aman <114105332+Aman-s12345@users.noreply.github.com>
2024-02-10 02:20:31 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -12,9 +12,10 @@ exports.connect = () => {
 		mongoose.connect("mongodb+srv://soulopscout:sfaman23@cluster0.druj2gv.mongodb.net/DrivingL", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+			dbName: 'DrivingL',
   retryWrites: true,
   w: "majority"
-});
+})
 		.then(console.log(`DB ka Connection Success`))
 		.catch((err) => {
 			console.log(`DB ka Connection Failed`);
